31cc47a485d6b4a650c12f890fc44c4c50df3a59
Igor Bondarenko <igor21v@mail.ru>
2024-04-23 22:55:59 +0800

    build: refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import { ScrollTrigger } from "gsap/all";
 // @ts-ignore
 import ScrollSmoother from "../assets/libs/gsap/ScrollSmoother.min.js";
 import "./App.css";
+import { Vertical } from "../pages/Vertical/Vertical";
 
 function App() {
   window.addEventListener("scroll", (e) => {
@@ -19,38 +20,7 @@ function App() {
       content: ".content",
     });
   });
-  return (
-    <div className="App">
-      <div className="wrapper">
-        <div className="content">
-          <header className="main-header">
-            <div className="layers">
-              <div className="layer__header">
-                <div className="layers__caption">Welcome to Parallax</div>
-                <div className="layers__title">Fairy Forest</div>
-              </div>
-              <div className="layer layers__base"></div>
-              <div className="layer layers__middle"></div>
-              <div className="layer layers__front"></div>
-            </div>
-          </header>
-
-          <article className="main-article">
-            <div className="main-article__content">
-              <h2 className="main-article__header">To be continued</h2>
-              <p className="main-article__paragraph">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis
-                error provident dignissimos facere. Repellendus tempore autem
-                qui! Quia magnam tempora esse id necessitatibus corrupti
-                mollitia expedita sapiente cum rerum, ut dicta laboriosam!
-              </p>
-            </div>
-            <div className="copy">Igor Bondarenko</div>
-          </article>
-        </div>
-      </div>
-    </div>
-  );
+  return <Vertical />;
 }
 
 export default App;
--- src/pages/Vertical/Vertical.tsx
+++ src/pages/Vertical/Vertical.tsx
@@ -1,8 +1,50 @@
-import { memo } from "react";
-import cls from "./vertical.module.scss";
+import { memo, useLayoutEffect } from "react";
+import cls from "./Vertical.module.scss";
 
 interface verticalProps {}
 
-export const vertical = memo((props: verticalProps) => {
-  return <div className={cls.vertical}></div>;
+export const Vertical = memo((props: verticalProps) => {
+  window.addEventListener("scroll", (e) => {
+    document.documentElement.style.setProperty(
+      "--scrollTop",
+      `${window.scrollY}px`
+    );
+  });
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    ScrollSmoother.create({
+      wrapper: ".wrapper",
+      content: ".content",
+    });
+  });
+  return (
+    <div className={cls.wrapper}>
+      <div className={cls.content}>
+        <header className={cls.main_header}>
+          <div className={cls.layers}>
+            <div className={cls.layer__header}>
+              <div className={cls.layers__caption}>Welcome to Parallax</div>
+              <div className={cls.layers__title}>Fairy Forest</div>
+            </div>
+            <div className={`${cls.layer} ${cls.layers__base}`}></div>
+            <div className={`${cls.layer} ${cls.layers__middle}`}></div>
+            <div className={`${cls.layer} ${cls.layers__front}`}></div>
+          </div>
+        </header>
+
+        <article className={cls.main_article}>
+          <div className={cls.main_article__content}>
+            <h2 className={cls.main_article__header}>To be continued</h2>
+            <p className={cls.main_article__paragraph}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis
+              error provident dignissimos facere. Repellendus tempore autem qui!
+              Quia magnam tempora esse id necessitatibus corrupti mollitia
+              expedita sapiente cum rerum, ut dicta laboriosam!
+            </p>
+          </div>
+          <div className={cls.copy}>Igor Bondarenko</div>
+        </article>
+      </div>
+    </div>
+  );
 });
