37e758976fb2da38dffb89bac86ed3c7653dae13
Igor Bondarenko <igor21v@mail.ru>
2024-04-25 00:09:46 +0800

    feat: setting typescript

--- src/shared/lib/ui/Button/Button.tsx
+++ src/shared/lib/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { ButtonHTMLAttributes, memo, ReactNode } from "react";
 import { classNames, Mods } from "shared/lib/classNames/classNames";
-import cls from "./Button.module.css";
+import cls from "./Button.module.scss";
 
 export type ButtonTheme =
   | "clear"
